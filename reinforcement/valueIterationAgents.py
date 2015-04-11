08c844aa8e9ed9c3842b466c3cee21c812a40c1c
Konstantinos Kanellis <kkanelli@hotmail.com>
2015-04-11 22:19:12 +0300

    reinforcement: Q1 6/6

--- reinforcement/valueIterationAgents.py
+++ reinforcement/valueIterationAgents.py
@@ -1,15 +1,15 @@
 # valueIterationAgents.py
 # -----------------------
-# Licensing Information:  You are free to use or extend these projects for 
-# educational purposes provided that (1) you do not distribute or publish 
-# solutions, (2) you retain this notice, and (3) you provide clear 
-# attribution to UC Berkeley, including a link to 
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to
 # http://inst.eecs.berkeley.edu/~cs188/pacman/pacman.html
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
-# The core projects and autograders were primarily created by John DeNero 
+# The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and 
+# Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
 
@@ -40,12 +40,23 @@ class ValueIterationAgent(ValueEstimationAgent):
               mdp.isTerminal(state)
         """
         self.mdp = mdp
+        self.T = mdp.getTransitionStatesAndProbs
+        self.R = mdp.getReward
         self.discount = discount
         self.iterations = iterations
         self.values = util.Counter() # A Counter is a dict with default 0
 
         # Write value iteration code here
-        "*** YOUR CODE HERE ***"
+        for _ in xrange(iterations):
+
+            self.values = {
+                state: max(
+                    [ sum(
+                        [ nextStateProb * ( self.R(state, action, nextState) + discount * self.values[nextState] ) \
+                                for (nextState, nextStateProb) in self.T(state, action) ]
+                    ) for action in mdp.getPossibleActions(state) ]
+                ) if not mdp.isTerminal(state) else self.values[state] for state in mdp.getStates()
+            }
 
 
     def getValue(self, state):
@@ -60,8 +71,9 @@ class ValueIterationAgent(ValueEstimationAgent):
           Compute the Q-value of action in state from the
           value function stored in self.values.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return sum( [nextStateProb * ( self.R(state, action, nextState) + self.discount * self.values[nextState] ) \
+                for (nextState, nextStateProb) in self.T(state, action)] )
+
 
     def computeActionFromValues(self, state):
         """
@@ -72,8 +84,21 @@ class ValueIterationAgent(ValueEstimationAgent):
           there are no legal actions, which is the case at the
           terminal state, you should return None.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+
+        if self.mdp.isTerminal(state):
+            return None
+
+        bestAction = 'NO_ACTION'
+        bestValue = -1e9
+        for action in self.mdp.getPossibleActions(state):
+            q_val = self.computeQValueFromValues(state, action)
+
+            if q_val > bestValue:
+                bestValue = q_val
+                bestAction = action
+
+        return bestAction
+
 
     def getPolicy(self, state):
         return self.computeActionFromValues(state)
